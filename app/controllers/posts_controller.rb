3668a3d17191c2a4167bf81bd3cef754be132374
ziadashraf0 <ziadshreet@gmail.com>
2022-08-20 15:51:14 +0200

    finished posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,40 @@
 class PostsController < ApplicationController
 
   def index
+    @posts=Post.all.order("created_at DESC")
+  end
+  def show
+    @post =Post.find(params[:id])
+  end
+  def update
+    @post=Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+  def edit
+    @post= Post.find(params[:id])
   end
   def create
-
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
   def new
-
+    @post=Post.new
+  end
+  def destroy
+    @post =Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path , status: :see_other
+  end
+  private
+  def post_params
+    params.require(:post).permit(:title,:content)
   end
 end
